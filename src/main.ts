706a07facf0f26c000503222b5dd0c6371b006b4
woog2roid <woog2roid@gmail.com>
2023-11-26 04:23:16 +0900

    setup: CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const document_config = new DocumentBuilder()
     .setTitle('Inthon Server')
