1d22c3f2674fb5602a853a571999b6ad0c71b38d
woog2roid <woog2roid@gmail.com>
2023-11-25 19:36:05 +0900

    setup: Add swagger, pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document_config = new DocumentBuilder()
+    .setTitle('Inthon Server')
+    .setDescription('Inthon API Description')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, document_config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
