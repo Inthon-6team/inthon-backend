6fdc113be9357cce8e297ef753ce11bdeba678a4
woog2roid <woog2roid@gmail.com>
2023-11-25 19:39:37 +0900

    setup: Connect with mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User],
+      migrations: [__dirname + '/src/migrations/*.ts'],
+      autoLoadEntities: true,
+      charset: 'utf8mb4',
+      synchronize: process.env.NODE_ENV !== 'production',
+      logging: process.env.NODE_ENV !== 'production',
+      keepConnectionAlive: true,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
